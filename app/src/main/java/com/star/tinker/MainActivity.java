430837f5e6aa592011dccda80b5f247049961bb7
xingxing <xxx823952375@gmail.com>
2017-05-03 21:48:07 +0800

    添加tinker配置文件

--- app/src/main/java/com/star/tinker/MainActivity.java
+++ app/src/main/java/com/star/tinker/MainActivity.java
@@ -2,6 +2,7 @@ package com.star.tinker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.star.tinker.R;
 
 public class MainActivity extends AppCompatActivity {
 
